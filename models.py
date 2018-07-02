2d9b3c2eaa939ce0e301631ba8f9525372b9cd9d
Michael Nthiwa <mike.nthiwa@gmail.com>
2018-07-02 13:22:09 +0300

    Adding models structure Adding gitignore

---
+++ models.py
@@ -0,0 +1,24 @@
+users = {}
+comments = {}
+
+class Users:
+    """Class contains users cases"""
+
+    def register(self):
+        pass
+
+    def login(self):
+        pass
+
+
+class Comments:
+    """class contains comments cases"""
+
+    def add_comment(self):
+        pass
+
+    def edit_comment(self):
+        pass
+
+    def delete_comment(self):
+        pass
